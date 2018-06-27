f5b79561f659c9432ab664e0b203298c5908dd7b
asshishkumar1550 <kumarashish1550@gmail.com>
2018-06-27 14:18:31 +0800

    changes

--- foodweb/settings.py
+++ foodweb/settings.py
@@ -39,7 +39,59 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #'social-django',
 ]
+
+# google login support
+"""
+AUTHENTICATION_BACKENDS = (
+ ‘social_core.backends.open_id.OpenIdAuth’,
+ ‘social_core.backends.google.GoogleOpenId’,
+ ‘social_core.backends.google.GoogleOAuth2’,
+ ‘django.contrib.auth.backends.ModelBackend’,
+)
+SOCIAL_AUTH_URL_NAMESPACE = ‘social’
+
+'context_processors': [
+
+...
+                'social_django.context_processors.backends',
+                'social_django.context_processors.login_redirect',
+            ],
+SOCIAL_AUTH_PIPELINE = (
+    'social.pipeline.social_auth.social_details',
+    'social.pipeline.social_auth.social_uid',
+    'social.pipeline.social_auth.auth_allowed',
+    'social.pipeline.social_auth.social_user',
+    'social.pipeline.user.get_username',
+    'social.pipeline.user.create_user',
+    'social.pipeline.social_auth.associate_user',
+    'social.pipeline.debug.debug',
+    'social.pipeline.social_auth.load_extra_data',
+    'social.pipeline.user.user_details',
+    'social.pipeline.debug.debug',
+)
+
+
+SOCIAL_AUTH_GOOGLE_OAUTH2_IGNORE_DEFAULT_SCOPE = True
+SOCIAL_AUTH_GOOGLE_OAUTH2_SCOPE = [
+'https://www.googleapis.com/auth/userinfo.email',
+'https://www.googleapis.com/auth/userinfo.profile'
+]
+
+# Google+ SignIn (google-plus)
+SOCIAL_AUTH_GOOGLE_PLUS_IGNORE_DEFAULT_SCOPE = True
+SOCIAL_AUTH_GOOGLE_PLUS_SCOPE = [
+'https://www.googleapis.com/auth/plus.login',
+'https://www.googleapis.com/auth/userinfo.email',
+'https://www.googleapis.com/auth/userinfo.profile'
+]
+
+LOGIN_URL = '/account/login/'
+"""
+# google login support
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY ='339959835608-h2jilk866s0qobbrqqp71u0j6jmta8lo.apps.googleusercontent.com'
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = 'm0SNrqjbzIjvLj7EfgoD3UAs'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
